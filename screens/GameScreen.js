d3704419784e931965e6f3de47216c56f12b907a
Lindsey Herman <lherman@onshift.com>
2024-01-29 14:37:57 -0500

    Caught up to vid69: Cascading Styles

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,90 +1,113 @@
-import { View, StyleSheet, Alert } from 'react-native';
-import Title from '../components/ui/Title';
-import NumberContainer from '../components/game/NumberContainer';
-import { useState, useEffect } from 'react';
-import PrimaryButton from '../components/ui/PrimaryButton';
-import Card from '../components/ui/Card';
-import InstructionText from '../components/ui/InstructionText';
+import { View, StyleSheet, Alert } from "react-native";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import { useState, useEffect } from "react";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
 
 //  exclude prevents the phone from guessing the number right away
 function generateRandomBetween(min, max, exclude) {
-    //  Math.floor() rounds the number down to the nearest whole #
-    //  since would get a decimal otherwise. Then need to add min
-    //  again since could get 0 otherwise but want the lowest number to be 1.
-    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  //  Math.floor() rounds the number down to the nearest whole #
+  //  since would get a decimal otherwise. Then need to add min
+  //  again since could get 0 otherwise but want the lowest number to be 1.
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
 
-    if (rndNum === exclude) {
-        //  Recursion!
-        return generateRandomBetween(min, max, exclude);
-    } else {
-        return rndNum;
-    }
+  if (rndNum === exclude) {
+    //  Recursion!
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
 }
 
 let minBoundary = 1;
 let maxBoundary = 100;
 
-function GameScreen({userNumber, onGameOver}) {
-    //  Biggest number should be 99 but need to go one higher for the Math.floor():
-    const initialGuess = generateRandomBetween(
-        //  These need to go back to 1 and 100 because of a rendering bug
-        //  or something? Something abt useEffect() vs useState()?
-        1, 
-        100, 
-        userNumber
-    );
-    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+function GameScreen({ userNumber, onGameOver }) {
+  //  Biggest number should be 99 but need to go one higher for the Math.floor():
+  const initialGuess = generateRandomBetween(
+    //  These need to go back to 1 and 100 because of a rendering bug
+    //  or something? Something abt useEffect() vs useState()?
+    1,
+    100,
+    userNumber
+  );
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
-    useEffect(() => {
-        if (currentGuess === userNumber) {
-            onGameOver();
-        }
-    }, [currentGuess, userNumber, onGameOver]);
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver]);
 
-    function nextGuessHandler(direction) {
-        if (
-            (direction === 'lower' && currentGuess < userNumber) ||
-            (direction === 'greater' && currentGuess > userNumber)
-        ) {
-            Alert.alert("Don't lie!", 'You know that this is wrong...', [
-                //  Don't need an onPress if all you want it to do is close
-                //  the Alert popup, button will do that automatically!
-                {text: 'Sorry!', style: 'cancel'}
-            ]);
-            return;
-        }
-
-        if (direction === 'lower') {
-            maxBoundary = currentGuess;
-        } else {
-            minBoundary = currentGuess + 1;
-        }
-        //  It wouldn't guess the currentGuess anyways but just putting that there.
-        const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
-        setCurrentGuess(newRndNumber);
+  function nextGuessHandler(direction) {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        //  Don't need an onPress if all you want it to do is close
+        //  the Alert popup, button will do that automatically!
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
     }
 
-    return (
-        <View style={styles.screen}>
-            <Title>Opponent's Guess</Title>
-            <NumberContainer>{currentGuess}</NumberContainer>
-            <Card>
-                <InstructionText>Higher or lower?</InstructionText>
-                <View>
-                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
-                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
-                </View>
-            </Card>
-            {/* <View>LOG ROUNDS</View> */}
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    //  It wouldn't guess the currentGuess anyways but just putting that there.
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card>
+        <InstructionText style={styles.instructionText}>
+          Higher or lower?
+        </InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+              -
+            </PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+              +
+            </PrimaryButton>
+          </View>
         </View>
-    )
+      </Card>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 24
-    },
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  instructionText: {
+    marginBottom: 12,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
 });
